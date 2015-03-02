04e63eb72f85582e93b3c18a2c7a45b087c90284
bhumitra nagar <nagar.b@husky.neu.edu>
2015-03-02 00:44:14 -0500

    commit week 6 partial

---
+++ essential/experiments/week6/js/script.js
@@ -0,0 +1,14 @@
+﻿angular.module( "Test", ['ngAutocomplete'])
+  .controller("TestCtrl",function ($scope) {
+
+    $scope.result1 = '';
+    $scope.options1 = null;
+    $scope.details1 = '';
+
+    $scope.result2 = '';
+    $scope.options2 = {
+      country: 'us',
+      types: '(cities)'
+    };    $scope.details2 = '';
+    
+  });
